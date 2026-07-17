998879c64ce027db55e16137511630620994aea2
Dan Nomen <dan.nomen@gmail.com>
2026-07-17 12:03:11 +0300

    Website Sale BXGY Reward Fixs

--- website_sale_bxgy_reward_fix/models/sale_order_line.py
+++ website_sale_bxgy_reward_fix/models/sale_order_line.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-from odoo import models
+from odoo import models, api
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
+
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    def _compute_price_unit(self):
+        super()._compute_price_unit()
+        for line in self:
+            if hasattr(line, 'is_bxgy_reward') and line.is_bxgy_reward:
+                line.price_unit = 0.0
 
     def _is_not_sellable_line(self):
         """
